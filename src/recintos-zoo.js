5bf8b5970b6d94f9fe54eabbe118368c9b08dba0
Thiago Sales <thiagosales46@gmail.com>
2024-09-12 00:14:22 -0300

    feat(recintosZoo): implementar análise de recintos para acomodar animais
    - Adicionado o método analisaRecintos para verificar se um animal pode ser alocado em um recinto.
    - Verifica biomas compatíveis, espaço disponível e restrições de animais carnívoros.
    - Filtra recintos viáveis e ordena por número.
    - Retorna os 3 melhores recintos disponíveis para acomodar a quantidade de animais.
    - Implementa validações para garantir que o animal e a quantidade sejam válidos.

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -34,6 +34,54 @@ class RecintosZoo {
     analisaRecintos(animal, quantidade) {
 
 
+        if (!this.animais[animal]) {
+            return {erro: 'Animal inválido'};
+        }
+
+        if (quantidade <= 0 || !Number.isInteger(quantidade)) {
+            return {erro: 'Quantidade inválida'};
+        }
+
+        const infoAnimal = this.animais[animal];
+        const tamanhoNecessario = infoAnimal.tamanho * quantidade;
+
+        const recintosViaveis = this.recintos.filter(recinto => {
+
+            const biomasRecinto = recinto.bioma.split(' e ');
+            const biomaAdequado = biomasRecinto.some(bioma => infoAnimal.biomas.includes(bioma));
+            if (!biomaAdequado) return false;
+
+            let espacoDisponivel = recinto.tamanhoTotal - recinto.ocupacaoAtual;
+
+            if (infoAnimal.carnivoro && recinto.animais.length > 0 && !recinto.animais.includes(animal)) {
+                return false;
+            }
+            let espacoOcupado = tamanhoNecessario;
+
+            if (recinto.animais.length > 0 && !recinto.animais.includes(animal)) {
+                espacoOcupado += 1;
+            }
+
+            return espacoDisponivel >= espacoOcupado;
+        });
+
+        if (recintosViaveis.length === 0) {
+            return {erro: 'Não há recinto viável'};
+        }
+
+        const recintosOrdenados = recintosViaveis.sort((a, b) => a.numero - b.numero);
+        const recintosMapeados = recintosOrdenados.map(recinto => {
+
+            let espacoOcupado = tamanhoNecessario;
+            if (recinto.animais.length > 0 && !recinto.animais.includes(animal)) espacoOcupado += 1;
+            const espacoLivre = recinto.tamanhoTotal - (recinto.ocupacaoAtual + espacoOcupado);
+            return `Recinto ${recinto.numero} (espaço livre: ${espacoLivre} total: ${recinto.tamanhoTotal})`;
+        });
+
+        return {recintosViaveis: recintosMapeados.slice(0, 3)};
+    }
+
+
 }
 
 export {RecintosZoo as RecintosZoo};
